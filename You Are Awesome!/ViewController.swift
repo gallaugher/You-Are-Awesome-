6005755419a8eed4a57c49fce7f99c86f0b2a4dd
John Gallaugher <john.gallaugher@bc.edu>
2020-01-07 11:32:54 -0500

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by John Gallaugher on 1/7/20.
+//  Copyright © 2020 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
